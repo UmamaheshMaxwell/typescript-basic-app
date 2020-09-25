000fc814cef785c40f5b0c8114c6b63293b56720
Umamaheswararao Meka <maheshmeka16@gmail.com>
2020-09-25 08:40:54 +0530

    added data types

---
+++ app.ts
@@ -0,0 +1,40 @@
+let myString: string;
+let myNumber: number;
+let myBoolean: boolean;
+let myVar: any
+
+let strArray: string[];
+let numArray: number[];
+let strNumTuple: [string, number];
+
+let empId: number = 1;
+let empName : string = "Uma"
+
+let employee: [number, string];
+
+
+
+
+myString = "Hello";
+myNumber = 1
+myBoolean = true;
+myVar = "Mahesh"
+myVar =1
+
+strArray = ["Uma", "mahesh" , "umesh"];
+numArray = [1,2,3,4]
+strNumTuple = ["Uma", 1]
+employee = [empId, empName]
+
+
+
+console.log(myString)
+console.log(myNumber)
+console.log(myBoolean)
+console.log(myVar)
+
+console.log(strArray)
+console.log(numArray)
+console.log(strNumTuple)
+console.log(employee)
+
